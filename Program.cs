3d830fd4aec26c99f17f7a9d9cea99e0018f3bb8
Gavin Lilley <g@gavinlilley.com>
2021-11-23 22:17:35 +0000

    update fluxor 4.2.1

--- Program.cs
+++ Program.cs
@@ -1,36 +1,25 @@
 using System;
 using System.Net.Http;
-using System.Collections.Generic;
-using System.Threading.Tasks;
-using System.Text;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
 using Fluxor;
 using System.Reflection;
 using BlazorFluxor.Services;
+using Microsoft.AspNetCore.Components.Web;
+using BlazorFluxor;
 
-namespace BlazorFluxor
+var builder = WebAssemblyHostBuilder.CreateDefault(args);
+builder.RootComponents.Add<App>("#app");
+builder.RootComponents.Add<HeadOutlet>("head::after");
+
+builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddFluxor(options =>
 {
-    public class Program
-    {
-        public static async Task Main(string[] args)
-        {
-            var builder = WebAssemblyHostBuilder.CreateDefault(args);
-            builder.RootComponents.Add<App>("#app");
+    options.ScanAssemblies(Assembly.GetExecutingAssembly());
+    options.UseReduxDevTools();
+});
 
-            builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.HostEnvironment.BaseAddress) });
+builder.Services.AddScoped<StateFacade>();
 
-            builder.Services.AddFluxor(options =>
-            {
-                options.ScanAssemblies(Assembly.GetExecutingAssembly());
-                options.UseReduxDevTools();
-            });
-
-            builder.Services.AddScoped<StateFacade>();
-
-            await builder.Build().RunAsync();
-        }
-    }
-}
+await builder.Build().RunAsync();
